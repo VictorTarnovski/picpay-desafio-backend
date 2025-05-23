599e3bebf30c3ef1fcfe1a32d7b89011391ee269
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2025-05-23 13:33:54 -0300

    feat: add UnauthorizedTransactionException

---
+++ src/main/java/com/picpay/transaction_processing/domain/exceptions/UnauthorizedTransactionException.java
@@ -0,0 +1,21 @@
+package com.picpay.transaction_processing.domain.exceptions;
+
+import com.picpay.shared.domain.exceptions.PicPayException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class UnauthorizedTransactionException extends PicPayException {
+    public UnauthorizedTransactionException() {
+        super("Unauthorized transaction exception");
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        var pb = super.toProblemDetail();
+
+        pb.setStatus(HttpStatus.FORBIDDEN);
+        pb.setTitle("Unauthorized transaction exception");
+
+        return pb;
+    }
+}
